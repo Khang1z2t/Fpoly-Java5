bb010789c1c193f7f2b70086eb6b5f7ec781c4af
yuno.k <120995031+Khang1z2t@users.noreply.github.com>
2024-11-25 21:21:22 +0700

    add Lab8

---
+++ PS36645_Lab8/src/main/java/com/java5/ps36645_lab8/Ps36645Lab8Application.java
@@ -0,0 +1,13 @@
+package com.java5.ps36645_lab8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps36645Lab8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ps36645Lab8Application.class, args);
+    }
+
+}
